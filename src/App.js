ab7b082de34ac6bcd1912e4e3b201361801fbdf4
Catriel Labat <zeeb.catriel.1992@gmail.com>
2022-04-04 18:16:23 -0300

    contador

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar/NavBar'
-import ItemListConteiner from './components/ItemListConteiner/ItemListConteiner';
+import ItemListConteiner from './components/ItemListConteiner/ItemListConteiner'
 import ItemCount from './components/ItemCount/ItemCount';
 
 function App() {
+
+  const  handleOnAdd = (quantify) =>{
+    console.log(`se agrego ${quantify} a nuestro carrito`);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <NavBar />
       </header>
     <ItemListConteiner greeting={'Bienvenidos'}/>
-    < ItemCount stock={5} initial={1} />  
+    < ItemCount initial={1} stock={9} onAdd={handleOnAdd}></ItemCount> 
     </div>
   );
 }
